41c9a2a5edacdb652d8af01c44c4584f0f442a90
raineer <delaritarainer81@gmail.com>
2018-06-24 12:16:10 +0800

    insert post 1

--- app.js
+++ app.js
@@ -26,7 +26,28 @@ app.get('/createdb', (req, res) => {
         console.log(result);
         res.send('Database created....');
     });
+}); 
+
+//create table
+app.get('/createPostsTable', (req, res) => {
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Posts table created....');
+    });
 });
+
+//insert post 1 
+app.get('/addpost1', (req, res) => {
+    let post = { title: 'Post one', body: 'This is post number one'};
+    let sql = 'Insert INTO posts SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post 1 added....');
+    });
+})
 
 app.listen('3000', () => {
     console.log('Server started on port 3000');
