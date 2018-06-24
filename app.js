a0f1c51f1f4824ca66c347894c5f97740cf231fc
raineer <delaritarainer81@gmail.com>
2018-06-24 13:18:52 +0800

    delete post

--- app.js
+++ app.js
@@ -80,6 +80,17 @@ app.get('/updatepost/:id', (req, res) => {
     });
 });
 
+//Delete post
+app.get('/deletepost/:id', (req, res) => {
+    let newTitle = 'Deleted title';
+    let sql = `DELETE FROM posts WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post Deleted....');
+    });
+});
+
 app.listen('3000', () => {
     console.log('Server started on port 3000');
 });
