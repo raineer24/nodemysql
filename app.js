db5582439cb166bbcde485174b7fc7e4cf8d0214
raineer <delaritarainer81@gmail.com>
2018-06-24 13:01:20 +0800

    update post

--- app.js
+++ app.js
@@ -69,6 +69,17 @@ app.get('/getpost/:id', (req, res) => {
     });
 });
 
+//Update post
+app.get('/updatepost/:id', (req, res) => {
+    let newTitle = 'Updated title';
+    let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post Updated....');
+    });
+});
+
 app.listen('3000', () => {
     console.log('Server started on port 3000');
 });
